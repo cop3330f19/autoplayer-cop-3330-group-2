7195171965cdf2662f2ad91b6ed700c069ca8b21
Amani Muller <54755290+mullera3@users.noreply.github.com>
2020-04-26 21:13:25 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -17,6 +17,7 @@
 #include <vector>
 #include "StringHelper.h"
 #include <cstdlib>  
+#include <algorithm>
 
 using namespace std;
 
@@ -25,6 +26,11 @@ const int countLinesInFile(string filename);
 
 // Adds song from file to song vector
 void openPlaylistFile(string filename, vector<string> & v);
+
+void writeNewPlaylistfile(string file, string playlist);
+
+void eraseDuplicates(vector<string> &v);
+
 
 // Static controlling playing mode
 int Playlist::mode = 0;
@@ -44,13 +50,16 @@ int main()
     int selection, selection1, selection2;
     char control, control2;
     
-    // holds playlist titles
     vector<string> play;
     
-    // adds playlist from file to vector
+     // adds playlist from file to vector
     openPlaylistFile("Playlist.list", play);
+ 
 do
 {
+      // adds playlist from file to vector
+    openPlaylistFile("Playlist.list", play);
+    eraseDuplicates(play);
 
     cout << "----------WELCOME to the AutoPlayer----------" << endl << endl;
     cout << "You currently have " << play.size() << " playlist(s)." << endl;
@@ -92,6 +101,7 @@ do
                 case 'A': 
                 cout << "Song Details" << endl;
                 cout << "Title: ";
+                cin.ignore();
                 getline(cin,title); //needs to be a getline
                 cout << "Artist: ";
                 getline(cin,artist);   
@@ -102,6 +112,7 @@ do
                 cout << "Length (in seconds): ";
                 cin >> song_in_seconds;
                 song.set(title,artist,album,song_in_seconds,year);
+                p.appendToFile(song);
                 p.addSong(song);
                 break;
                 case 'D':
@@ -148,13 +159,13 @@ do
         cout << "3 - Intersect 2 exisinting playlists" << endl;
         cout << "Selection: ";
         cin >> selection1;
-        cout << "Name of new playlist (cannot contain underscores):";
+        cout << "Name of new playlist (cannot contain underscores): ";
         cin.ignore();
         getline(cin,newPlaylist);
         cout << endl;
          if(selection1 == 1)
         {
-            play.push_back(newPlaylist); 
+            writeNewPlaylistfile("Playlist.list", newPlaylist);
         }
         if(selection1 == 2)
        {
@@ -163,39 +174,42 @@ do
            {
                cout <<  i + 1 << " " << play[i] << endl;
            }
+           Playlist p3(newPlaylist);
+           writeNewPlaylistfile("Playlist.list", newPlaylist);
            cout << "Playlist 1: ";
            cin >> selection1;
            cout << endl;
            choice = play[selection1-1];
            Playlist p(choice);
+           p3.merge(p);
            cout << "Playlist 2: ";
            cin >> selection2;
            cout << endl;
            choice2 = play[selection2-1];
            Playlist p1(choice2);
-           Playlist p3(newPlaylist);
            p3.merge(p1);
            p3.merge(p);
        }
-       if(selection == 3)
+       if(selection1 == 3)
        {
            cout << "Which of the following playlists would you like to intersect? " << endl; 
            for(int i = 0; i < play.size(); i++)
            {
                cout <<  i + 1 << " " << play[i] << endl;
            }
+           Playlist p3(newPlaylist);
+           writeNewPlaylistfile("Playlist.list", newPlaylist);
            cout << "Playlist 1: ";
            cin >> selection1;
            cout << endl;
            choice = play[selection1-1];
            Playlist p(choice);
+           p3  = p;
            cout << "Playlist 2: ";
            cin >> selection2;
            cout << endl;
            choice2 = play[selection2-1];
            Playlist p1(choice2);
-           Playlist p3(newPlaylist);
-           p3.intersect(p);
            p3.intersect(p1);
        }
     }// case 2 end
@@ -233,5 +247,24 @@ void openPlaylistFile(string filename, vector <string> & v)
     ifstream file;
     file.open(filename.c_str());
     while(file >> line)
+    {
         v.push_back(line);
+       
+    }
+
+       
+}
+
+void writeNewPlaylistfile(string file, string playlist)
+{
+
+    ofstream f;
+    f.open(file.c_str(),ios::app);
+    f << '\n' << playlist;
+}
+
+void eraseDuplicates(vector<string> & v)
+{
+   sort(v.begin(), v.end());
+  v.erase(unique(v.begin(), v.end()), v.end());
 }
